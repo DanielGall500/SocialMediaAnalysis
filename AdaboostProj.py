35d251874b6f5c48ce66a370a2cf55bdeb8890fa
DannyGotTea <danielgallagher500@gmail.com>
2016-04-29 20:51:44 +0100

    Dataset imported

--- AdaboostProj.py
+++ AdaboostProj.py
@@ -1,3 +1,9 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import AdaBoostClassifier
+
+dataset = "/Users/dannyg/Dropbox/Datasets/SocialMediaBuzzDataset/Twitter/Absolute_labeling/Twitter-Absolute-Sigma-500.csv"
+
+data = pd.read_csv(dataset, sep=',')
+
+print (data)
