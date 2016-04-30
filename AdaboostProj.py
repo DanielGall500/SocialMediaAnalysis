068cbfec4f1bfeb98c3ac7988b5a62d06d8fce9c
DannyGotTea <danielgallagher500@gmail.com>
2016-04-30 12:43:39 +0100

    Cross validation complete

--- AdaboostProj.py
+++ AdaboostProj.py
@@ -6,4 +6,9 @@ dataset = "/Users/dannyg/Dropbox/Datasets/SocialMediaBuzzDataset/Twitter/Absolut
 
 data = pd.read_csv(dataset, sep=',')
 
-print (data)
+from sklearn.cross_validation import train_test_split
+
+X = data.ix[: , :76] #features
+Y = data.ix[: , 77] #buzz or no buzz
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
